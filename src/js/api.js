62fe694d6c3167d5f20c82cca2689149952e9426
OlyaArta <artanika8@gmail.com>
2021-08-18 14:26:49 +0300

    add parcel and fixed deploy

--- src/js/api.js
+++
@@ -1,7 +0,0 @@
-function fetchCountry(searchQuery) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`) 
-    .then(response => 
-         response.json());
-}
-
-export default { fetchCountry };
