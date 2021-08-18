abb8ae3d849eaf0251c13df0afba6f8a53a8f630
OlyaArta <artanika8@gmail.com>
2021-08-18 15:16:58 +0300

    finally fixed

---
+++ src/js/api.js
@@ -0,0 +1,7 @@
+function fetchCountry(searchQuery) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`) 
+    .then(response => 
+         response.json());
+}
+
+export default { fetchCountry };
